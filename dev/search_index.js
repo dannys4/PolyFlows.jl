5c20f62fb0ec419eddf68fc8c401edf2eba0ef06
Documenter.jl <documenter@juliadocs.github.io>
2025-04-28 19:31:51 +0000

    build based on 16e3765

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PolyFlows","page":"Home","title":"PolyFlows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PolyFlows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
